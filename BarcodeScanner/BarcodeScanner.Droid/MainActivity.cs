59c64795222f6b360d3cc9dacac10d7944368e9a
Rob Prouse <rob@prouse.org>
2016-03-15 09:21:09 -0400

    Added a simple barcode scanner

--- BarcodeScanner/BarcodeScanner.Droid/MainActivity.cs
+++ BarcodeScanner/BarcodeScanner.Droid/MainActivity.cs
@@ -1,10 +1,6 @@
-﻿using System;
-
+﻿
 using Android.App;
 using Android.Content.PM;
-using Android.Runtime;
-using Android.Views;
-using Android.Widget;
 using Android.OS;
 
 namespace BarcodeScanner.Droid
@@ -18,6 +14,11 @@ namespace BarcodeScanner.Droid
 
          global::Xamarin.Forms.Forms.Init(this, bundle);
          LoadApplication(new App());
+      }
+
+      public override void OnRequestPermissionsResult(int requestCode, string[] permissions, Permission[] grantResults)
+      {
+         global::ZXing.Net.Mobile.Forms.Android.PermissionsHandler.OnRequestPermissionsResult(requestCode, permissions, grantResults);
       }
    }
 }
---
+++ BarcodeScanner/BarcodeScanner/HomePage.cs
@@ -0,0 +1,48 @@
+﻿
+using Xamarin.Forms;
+using ZXing.Net.Mobile.Forms;
+
+namespace BarcodeScanner
+{
+   public class HomePage : ContentPage
+   {
+      ZXingScannerPage scanPage;
+
+      public HomePage()
+      {
+         var label = new Label
+         {
+            HorizontalOptions = LayoutOptions.CenterAndExpand,
+            VerticalOptions = LayoutOptions.CenterAndExpand,
+            FontSize = Device.GetNamedSize(NamedSize.Large, typeof(Label))
+         };
+         var button = new Button
+         {
+            Text = "Scan",
+            HorizontalOptions = LayoutOptions.FillAndExpand
+         };
+
+         Content = new StackLayout
+         {
+            Children = { label, button  }
+         };
+
+         button.Clicked += async delegate {
+            scanPage = new ZXingScannerPage
+            {
+               DefaultOverlayShowFlashButton = true
+            };
+            scanPage.OnScanResult += (result) => {
+               scanPage.IsScanning = false;
+
+               Device.BeginInvokeOnMainThread(async () => {
+                  await Navigation.PopAsync();
+                  label.Text = result.Text;
+               });
+            };
+
+            await Navigation.PushAsync(scanPage);
+         };
+      }
+   }
+}
