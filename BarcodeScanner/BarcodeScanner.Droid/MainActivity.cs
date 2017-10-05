60f385d8c27c9be8c9fc0ba8a432a7d34962c42c
Rob Prouse <rob.prouse@bentley.com>
2017-10-05 10:36:22 -0400

    Fix obsolete warning

--- BarcodeScanner/BarcodeScanner.Droid/MainActivity.cs
+++ BarcodeScanner/BarcodeScanner.Droid/MainActivity.cs
@@ -18,7 +18,7 @@ namespace BarcodeScanner.Droid
 
       public override void OnRequestPermissionsResult(int requestCode, string[] permissions, Permission[] grantResults)
       {
-         global::ZXing.Net.Mobile.Forms.Android.PermissionsHandler.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+         global::ZXing.Net.Mobile.Android.PermissionsHandler.OnRequestPermissionsResult(requestCode, permissions, grantResults);
       }
    }
 }
